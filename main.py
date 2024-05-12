04ec715df845c4f306a75af823bddec7c91e9f91
arttem11 <artemkuznecov900500@gmail.com>
2024-05-12 15:25:25 +0500

    Add files via upload
    v1.0

---
+++ main.py
@@ -0,0 +1,161 @@
+from PyQt5.QtWidgets import QWidget, QFileDialog, QListWidget, QApplication, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+import os
+from PIL import Image
+from PIL import ImageFilter
+
+app = QApplication([])
+win = QWidget()       
+win.resize(700, 500) 
+win.setWindowTitle('Easy Editor')
+lb_image = QLabel("Картинка")
+btn_dir = QPushButton("Папка")
+lw_files = QListWidget()
+btn_left = QPushButton("Лево")
+btn_right = QPushButton("Право")
+btn_flip = QPushButton("Зеркало")
+btn_sharp = QPushButton("Резкость")
+btn_bw = QPushButton("Ч/Б")
+
+row = QHBoxLayout()          # Основная строка
+col1 = QVBoxLayout()         # делится на два столбца
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)      # в первом - кнопка выбора директории
+col1.addWidget(lw_files)     # и список файлов
+row_tools = QHBoxLayout()    # и строка кнопок
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addWidget(lb_image, 95) # вo втором - картинка
+col2.addLayout(row_tools)
+
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+win.setLayout(row)
+
+win.show()
+
+workdir = ''
+
+def chooseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+def filter(files, extensions):
+    result = []
+    for i in files:
+        for g in extensions:
+            if i.endswith(g):
+                result.append(i)
+    return result
+
+def showFilenameList():
+    extensions = [ '.jpg', '.png', '.bmp', '.gif', '.jpeg']
+    chooseWorkdir()
+    filenames = filter(os.listdir(workdir), extensions)
+    lw_files.clear()
+    for k in filenames:
+#        lw_files.addItem(filenames)
+        lw_files.addItem(k)
+
+btn_dir.clicked.connect(showFilenameList)
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.filename = None
+        self.save_dir = "Modifed/"
+
+
+    def loadImage(self, filename):
+        self.filename = filename
+        image_path = os.path.join(workdir, filename)
+        self.image= Image.open(image_path)
+
+
+    def showImage(self, path):
+        lb_image.hide()
+        pixmapimage = QPixmap(path)
+        w, h = lb_image.width(), lb_image.height()
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        lb_image.setPixmap(pixmapimage)
+        lb_image.show()
+
+
+    def do_bw(self):
+        self.image = self.image.convert("L")
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+    def saveImage(self):
+        path = os.path.join(workdir, self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
+
+
+    def do_flip(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+    def do_left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+    
+    def do_right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+    def do_sparpen(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+
+
+
+
+workimage = ImageProcessor()
+
+
+def showChosenImage():
+    if lw_files.currentRow() >= 0:
+        filename = lw_files.currentItem().text()
+        workimage.loadImage(filename)
+        image_path = os.path.join(workdir, workimage.filename)
+        workimage.showImage(image_path)
+
+
+lw_files.currentRowChanged.connect(showChosenImage)
+btn_bw.clicked.connect(workimage.do_bw)
+btn_flip.clicked.connect(workimage.do_flip)
+btn_left.clicked.connect(workimage.do_left)
+btn_right.clicked.connect(workimage.do_right)
+btn_sharp.clicked.connect(workimage.do_sparpen)
+
+app.exec()
+
+
+
+
+
+
+
+
